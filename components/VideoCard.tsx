38d756b925e0c51d80655d9c3a5c56c7257deca3
Rushwayc <rushwayc@yahoo.com>
2022-12-13 21:24:27 +0100

    Added Video types and finished index.tsx. Starting VideoCard.

---
+++ components/VideoCard.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect, useRef } from 'react'
+import { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs'
+import { GoVerified } from 'react-icons/go'
+
+import { Video } from '../types'
+
+interface IProps {
+    post: Video
+}
+
+const VideoCard: NextPage<IProps> = ({ post }) => {
+  return (
+    <div>VideoCard</div>
+  )
+}
+
+export default VideoCard
