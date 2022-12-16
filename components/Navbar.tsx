12b302152e1af28d20e6ecc3c61cb4df89b53813
Rushwayc <rushwayc@yahoo.com>
2022-12-16 17:49:41 +0100

    Fixing Tailwind CSS. Ran: git config --local core.autocrlf false

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,10 +8,13 @@ import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 
 import Logo from '../utils/tiktik-logo.png'
+import { createOrGetUser } from '../utils'
+import useAuthStore from '../store/authStore'
 
 const Navbar = () => {
 
-  const user = false
+  //From zustand in ../store/authStore.ts
+  const { userProfile, addUser } = useAuthStore()
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -29,13 +32,36 @@ const Navbar = () => {
         SEARCH
       </div>
       <div>
-        {user ? (
-          <div>
-            LOGGED IN
+        {userProfile ? (
+          <div className='flex gap-5 md:gap-10'>
+              {/** Upload button. */}
+              <Link href='/upload'>
+                <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                  <IoMdAdd className='text-xl' /> {` `}
+                  <span className='hidden md:block'>Upload</span>
+                </button>
+              </Link>
+
+              {/** User profile image. */}
+              {userProfile.image && (
+                <Link href='/' >
+                  <>
+                    <Image
+                      width={40}
+                      height={40}
+                      className='rounded-full cursor-pointer'
+                      //
+                      src={userProfile.image}
+                      alt="profile photo"
+                    />
+                  </>
+                </Link>
+              )}
           </div>
         ) : (
           <GoogleLogin
-            onSuccess={(response) => console.log(response)}
+            //onSuccess of GoogleLogin get the data to utils index.
+            onSuccess={(response) => createOrGetUser(response, addUser)}
             onError={() => console.log('Error.')}
           />
         )}
--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,23 @@
 import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
+//Creates or gets user from Sanity database.
+//Second param is for Zustand.
 export const createOrGetUser = async (response: any, addUser: any) => {
-  var base64Url = response.credential.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-  
-};
+  const decoded: {name: string, picture: string, sub: string} = jwtDecode(response.credential)
+
+  const { name, picture, sub } = decoded
+
+  const user = {
+    _id: sub,
+    _type: 'user',
+    userName: name,
+    image: picture
+  }
+
+  //For Zustand.
+  addUser(user)
+
+  //Post results from Google loging to Sanity.
+  await axios.post(`http://localhost:3000/api/auth`, user)
+}
